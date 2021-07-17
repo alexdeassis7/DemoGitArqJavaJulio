9cedc84f2e059844c3e802c49a08a750cbd63edb
Alex De assis <alexdeassis7@gmail.com>
2021-07-17 17:23:28 -0300

    se soluciona el bug 123456 toco trabajar

--- SpringAnotationsClase2/src/com/interfaces/CreacionInformesFinancieros.java
+++ SpringAnotationsClase2/src/com/interfaces/CreacionInformesFinancieros.java
@@ -1,5 +1,6 @@
 package com.interfaces;
 
+//bueno ya realice la mejora solicitada por mi jefecito 
 public interface CreacionInformesFinancieros {
 
 	public String getInformeFinanciero();
--- SpringAnotationsClase2/src/com/interfaces/IEmpleados.java
+++ SpringAnotationsClase2/src/com/interfaces/IEmpleados.java
@@ -1,5 +1,5 @@
 package com.interfaces;
-
+//hoy me toco laburar , me pagan poco y me exigen mucho esto asi no va mas 
 //creamos una abstraccion , que tiene el metodo getTareas() y getInforme()
 public interface IEmpleados {
 	public String getTareas();
